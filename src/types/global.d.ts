dff88280e27a58e8ed93583cbb49a46536bd4ef3
tamurakeito <tamurakeito@chibikei-Mac-mini.local>
2025-01-15 20:56:57 +0900

    yarnv1->v4 + submodule化

---
+++ src/types/global.d.ts
@@ -0,0 +1,23 @@
+declare module "*.scss" {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
+// declare module "*.svg" {
+//   const content: string;
+//   export default content;
+// }
+// declare module "*.png" {
+//   const content: string;
+//   export default content;
+// }
+
+// declare module "*.wav" {
+//   const content: string;
+//   export default content;
+// }
+
+// declare module "*.m4a" {
+//   const content: string;
+//   export default content;
+// }
