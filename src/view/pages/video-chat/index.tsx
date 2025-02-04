e08519d0f662745ee6d7c9b30963db9571f775da
tamurakeito <kit_tamtam@icloud.com>
2025-02-04 17:16:47 +0900

    web-rtc テキストチャットの実装

--- src/view/pages/video-chat/index.tsx
+++ src/view/pages/video-chat/index.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import classes from "./styles.module.scss";
 import {
   Button,
   Center,
+  Input,
   Text,
   buttonSizes,
   textColors,
@@ -10,20 +11,24 @@ import {
 import { CircleButton } from "./circle-button";
 import { LogOut, Mic, MicOff, Video, VideoOff } from "react-feather";
 import Spinner from "./spinner";
+import classNames from "classnames";
 
 export const VideoChat = () => {
   const localVideoRef = useRef<HTMLVideoElement>(null);
   const remoteVideoRef = useRef<HTMLVideoElement>(null);
   const peerConnectionRef = useRef<RTCPeerConnection | null>(null);
+  const dataChannelRef = useRef<RTCDataChannel | null>(null);
   const socketRef = useRef<WebSocket | null>(null);
   const clientId = useRef<string>(crypto.randomUUID());
 
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
-  const [isChatReady, setIsChatReady] = useState(false);
+  const [isVideoReady, setIsVideoReady] = useState(false);
   const [isRemoteVideoReady, setIsRemoteVideoReady] = useState(false);
   const [isLocalVideoRendered, setIsLocalVideoRendered] = useState(false);
   const [isMuted, setIsMuted] = useState(false);
   const [isCameraOff, setIsCameraOff] = useState(false);
+  const [message, setMessage] = useState("");
+  const [chatArray, setChatArray] = useState<Array<ReactNode>>([]);
 
   // リモートSDP設定前に送られたICE候補を一時的に保存するバッファ
   const candidateBuffer: RTCIceCandidate[] = [];
@@ -86,11 +91,10 @@ export const VideoChat = () => {
       }
     };
 
+    // 接続の終了を監視
     peerConnection.onconnectionstatechange = () => {
-      setIsRemoteVideoReady(
-        peerConnection.connectionState !== "disconnected" &&
-          peerConnection.connectionState !== "failed"
-      );
+      const state = peerConnection.connectionState;
+      setIsRemoteVideoReady(state !== "disconnected" && state !== "failed");
     };
 
     return peerConnection;
@@ -116,15 +120,18 @@ export const VideoChat = () => {
         peerConnection.addTrack(track, stream);
       });
 
+      // データチャネルを作成
+      dataChannelRef.current =
+        peerConnection.createDataChannel("myDataChannel");
+      recieveMessage();
+
       // SDPの生成
       const offer = await peerConnection.createOffer();
       await peerConnection.setLocalDescription(offer);
 
-      // SDPを保存
-      // setLocalSDP(offer.sdp || "");
-
       // SDPを送信
       if (socketRef.current) {
+        console.log("offer送信");
         socketRef.current.send(
           JSON.stringify({
             type: "offer",
@@ -139,19 +146,21 @@ export const VideoChat = () => {
   };
 
   const handleRemoteSDP = async (sdp: string, type: "offer" | "answer") => {
+    const peerConnection = peerConnectionRef.current;
+
     // 現在のRTCPeerConnectionのインスタンスが存在しない場合は処理を終了
-    if (!peerConnectionRef.current) return;
+    if (!peerConnection) return;
 
     // 受信したSDPをリモートのセッション記述として設定する
     const remoteDesc = new RTCSessionDescription({ type, sdp });
     console.log("Remote Description:", remoteDesc);
     // リモートSDPの内容が接続の一部として適用される
-    await peerConnectionRef.current.setRemoteDescription(remoteDesc);
+    await peerConnection.setRemoteDescription(remoteDesc);
 
     // リモートSDPが設定された後にバッファ内のICE候補を処理
     for (const candidate of candidateBuffer) {
       try {
-        await peerConnectionRef.current.addIceCandidate(candidate);
+        await peerConnection.addIceCandidate(candidate);
       } catch (error) {
         console.error("Error adding buffered ICE candidate:", error);
       }
@@ -161,14 +170,20 @@ export const VideoChat = () => {
 
     if (type === "offer") {
       // SDP（answer）を生成
-      const answer = await peerConnectionRef.current.createAnswer();
+      const answer = await peerConnection.createAnswer();
       // 生成したanserをローカルセッション記述として設定する
-      await peerConnectionRef.current.setLocalDescription(answer);
+      await peerConnection.setLocalDescription(answer);
 
-      // setLocalSDP(answer.sdp || "");
+      // 受信したデータチャネルに置き換える
+      peerConnection.ondatachannel = (event) => {
+        dataChannelRef.current = event.channel;
+        recieveMessage();
+      };
+      console.log("client B として受信しました");
 
       // SDPを送信
       if (socketRef.current) {
+        console.log("answer送信");
         socketRef.current.send(
           JSON.stringify({
             type: "answer",
@@ -207,10 +222,10 @@ export const VideoChat = () => {
       if (message.clientId === clientId.current) return;
 
       if (message.type === "offer") {
-        // setRemoteSDP(message.sdp);
+        console.log("offer 受信");
         handleRemoteSDP(message.sdp, "offer");
       } else if (message.type === "answer") {
-        // setRemoteSDP(message.sdp);
+        console.log("answer 受信");
         handleRemoteSDP(message.sdp, "answer");
       } else if (message.type === "candidate") {
         handleNewICECandidate(message.data);
@@ -238,85 +253,157 @@ export const VideoChat = () => {
     }
   };
 
+  // 任意のメッセージをdataChannelを経由して送信する
+  const sendMessage = (message: string) => {
+    if (
+      dataChannelRef.current &&
+      dataChannelRef.current.readyState === "open"
+    ) {
+      dataChannelRef.current.send(message);
+      setChatArray((prev) => [
+        ...prev,
+        <ChatNode isSender={true}>{message}</ChatNode>,
+      ]);
+    } else {
+      console.log("データチャネルが開いていません");
+    }
+  };
+
+  const recieveMessage = () => {
+    const dataChannel = dataChannelRef.current;
+
+    if (!dataChannel) return;
+
+    dataChannel.onmessage = (event: MessageEvent) => {
+      setChatArray((prev) => [
+        ...prev,
+        <ChatNode isSender={false}>{event.data}</ChatNode>,
+      ]);
+    };
+
+    // クリーンアップ: dataChannel が変更されたら古いリスナーを削除
+    return () => {
+      dataChannel.onmessage = null;
+    };
+  };
+
   return (
-    <div
-      className={classes.video_chat}
-      style={{ width: screenWidth, height: screenWidth * 0.6 }}
-    >
-      {!isChatReady && (
-        <Center className={classes.chat_standby}>
-          <Button
+    <>
+      <div
+        className={classes.video_chat}
+        style={{ width: screenWidth - 20, height: screenWidth * 0.6 - 12 }}
+      >
+        {!isVideoReady && (
+          <Center className={classes.video_standby}>
+            <Button
+              onClick={() => {
+                startCall();
+                setIsVideoReady(true);
+              }}
+            >
+              <Text color={textColors.white}>ビデオ通話を開始します</Text>
+            </Button>
+          </Center>
+        )}
+        <video
+          ref={localVideoRef}
+          className={classes.local_video}
+          autoPlay
+          playsInline
+          muted
+          style={{ display: !isCameraOff ? "block" : "none" }}
+        />
+        {!isLocalVideoRendered && (
+          <Center className={classes.local_standby}>
+            <Spinner size={16} />
+          </Center>
+        )}
+        {isCameraOff && (
+          <Center className={classes.local_standby}>
+            <Text color={textColors.white}>画面オフ</Text>
+          </Center>
+        )}
+        <video
+          ref={remoteVideoRef}
+          className={classes.remote_video}
+          autoPlay
+          playsInline
+          style={{ display: isRemoteVideoReady ? "block" : "none" }}
+        />
+        {!isRemoteVideoReady && (
+          <Center className={classes.remote_standby}>
+            <Text color={textColors.white}>
+              接続されるまで少々お待ちください
+            </Text>
+          </Center>
+        )}
+        {isMuted && (
+          <div className={classes.mute_icon}>
+            <MicOff size={24} color={"grey"} />
+          </div>
+        )}
+        <div className={classes.controls}>
+          <CircleButton
+            size={buttonSizes.md}
             onClick={() => {
-              startCall();
-              setIsChatReady(true);
+              setIsMuted(!isMuted);
             }}
           >
-            <Text color={textColors.white}>ビデオ通話を開始します</Text>
-          </Button>
-        </Center>
-      )}
-      <video
-        ref={localVideoRef}
-        className={classes.local_video}
-        autoPlay
-        playsInline
-        muted
-        style={{ display: !isCameraOff ? "block" : "none" }}
-      />
-      {!isLocalVideoRendered && (
-        <Center className={classes.local_standby}>
-          <Spinner size={16} />
-        </Center>
-      )}
-      {isCameraOff && (
-        <Center className={classes.local_standby}>
-          <Text color={textColors.white}>画面オフ</Text>
-        </Center>
-      )}
-      <video
-        ref={remoteVideoRef}
-        className={classes.remote_video}
-        autoPlay
-        playsInline
-        style={{ display: isRemoteVideoReady ? "block" : "none" }}
-      />
-      {!isRemoteVideoReady && (
-        <Center className={classes.remote_standby}>
-          <Text color={textColors.white}>接続されるまで少々お待ちください</Text>
-        </Center>
-      )}
-      {isMuted && (
-        <div className={classes.mute_icon}>
-          <MicOff size={24} color={"grey"} />
+            {!isMuted ? <MicOff /> : <Mic />}
+          </CircleButton>
+          <CircleButton
+            size={buttonSizes.md}
+            onClick={() => {
+              setIsCameraOff(!isCameraOff);
+            }}
+          >
+            {!isCameraOff ? <VideoOff /> : <Video />}
+          </CircleButton>
+          <CircleButton
+            size={buttonSizes.md}
+            disabled={!isRemoteVideoReady}
+            onClick={() => {
+              endCall();
+            }}
+          >
+            <LogOut />
+          </CircleButton>
         </div>
-      )}
-      <div className={classes.controls}>
-        <CircleButton
-          size={buttonSizes.md}
-          onClick={() => {
-            setIsMuted(!isMuted);
-          }}
-        >
-          {!isMuted ? <MicOff /> : <Mic />}
-        </CircleButton>
-        <CircleButton
-          size={buttonSizes.md}
-          onClick={() => {
-            setIsCameraOff(!isCameraOff);
-          }}
-        >
-          {!isCameraOff ? <VideoOff /> : <Video />}
-        </CircleButton>
-        <CircleButton
-          size={buttonSizes.md}
-          disabled={!isRemoteVideoReady}
-          onClick={() => {
-            endCall();
-          }}
-        >
-          <LogOut />
-        </CircleButton>
+        <div className={classes.chat_area}>
+          {chatArray.map((data, index) => (
+            <span key={index}>{data}</span>
+          ))}
+        </div>
       </div>
+      <Center>
+        <Input
+          value={message}
+          placeholder={"メッセージを入力してください"}
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyDown={(event) => {
+            if (event.key === "Enter") {
+              sendMessage(message);
+              setMessage("");
+            }
+          }}
+        />
+      </Center>
+    </>
+  );
+};
+
+const ChatNode = ({
+  children,
+  isSender,
+}: {
+  children: ReactNode;
+  isSender: boolean;
+}) => {
+  return (
+    <div className={classNames([classes.chat_node])}>
+      <Text color={isSender ? textColors.gray500 : textColors.gray100}>
+        {children}
+      </Text>
     </div>
   );
 };
